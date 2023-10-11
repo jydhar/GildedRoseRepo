8e06e9aee00fcf96ef48c5ebf0c845e6069ea428
jyotisree <jyotisreed@gmail.com>
2023-10-11 12:53:37 +0530

    final submission removed some more nested if statements

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,16 +21,15 @@ class GildedRose {
 
     private void updateEachItemQuality(Item item) {
         int downRate = item.name.equals(ItemType.CONJURED_MANA_CAKE.getName()) ? -2 : -1;
+
         if (!item.name.equals(ItemType.AGED_BRIE.getName())
                 && !item.name.equals(ItemType.BACKSTAGE_PASSES_TAFKAL80ETC_CONCERT.getName()) && !item.name.equals(ItemType.SULFURAS_HAND_RAGNAROS.getName())) {
-            updateQualityCounter(item,downRate);
+            updateQualityCounter(item, downRate);
         } else {
-            updateQualityCounter(item,1);
+            updateQualityCounter(item, 1);
 
-            if (item.name.equals(ItemType.BACKSTAGE_PASSES_TAFKAL80ETC_CONCERT.getName())) {
-                if (item.sellIn < 11 || item.sellIn < 6) {
-                    updateQualityCounter(item,1);
-                }
+            if (item.name.equals(ItemType.BACKSTAGE_PASSES_TAFKAL80ETC_CONCERT.getName()) && (item.sellIn < 11 || item.sellIn < 6)) {
+                updateQualityCounter(item, 1);
             }
         }
 
@@ -40,19 +39,18 @@ class GildedRose {
 
         if (item.sellIn < 0) {
             if (!item.name.equals(ItemType.AGED_BRIE.getName())) {
-                if (!item.name.equals(ItemType.BACKSTAGE_PASSES_TAFKAL80ETC_CONCERT.getName())) {
-                    if (!item.name.equals(ItemType.SULFURAS_HAND_RAGNAROS.getName())) {
-                        updateQualityCounter(item,downRate);
-                    }
+                if (!item.name.equals(ItemType.BACKSTAGE_PASSES_TAFKAL80ETC_CONCERT.getName()) || !item.name.equals(ItemType.SULFURAS_HAND_RAGNAROS.getName())) {
+                    updateQualityCounter(item, downRate);
                 } else {
                     item.quality = item.quality - item.quality;
                 }
             } else {
-                updateQualityCounter(item,1);
+                updateQualityCounter(item, 1);
             }
         }
     }
-   private void updateQualityCounter(Item item, int counter) {
+
+    private void updateQualityCounter(Item item, int counter) {
         int updatedQuality = item.quality + counter;
         boolean isItValid = updatedQuality <= 50 && updatedQuality >= 0;
         if (isItValid) {
